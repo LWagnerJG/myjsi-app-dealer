46981332b02afd997c0eb25fce087a34f7120070
Luke Wagner <LWagner@jaspergroup.us.com>
2026-06-15 11:24:35 -0400

    Remove sample discount policies

--- src/screens/resources/sample-discounts/data.js
+++ src/screens/resources/sample-discounts/data.js
@@ -13,16 +13,6 @@ export const SAMPLE_POLICIES = [
     ],
   },
   {
-    id: 'rep-showroom',
-    title: 'Rep Showroom & Rep Samples',
-    ssa: 'RT-711576',
-    discount: 85,
-    notes: [
-      'Max qty (1) per model number',
-      'Commission is not paid',
-    ],
-  },
-  {
     id: 'dealer-showroom',
     title: 'Dealer Showroom Samples',
     ssa: 'DR-996860',
@@ -37,15 +27,6 @@ export const SAMPLE_POLICIES = [
     title: 'Personal Use',
     subtitle: 'Reps, Dealers, Designers & Internal Employees',
     ssa: 'DF-022745',
-    discount: 75,
-    notes: [
-      'Commission is not paid',
-    ],
-  },
-  {
-    id: 'design-firms',
-    title: 'Design Firms',
-    ssa: 'WE-304039',
     discount: 75,
     notes: [
       'Commission is not paid',
