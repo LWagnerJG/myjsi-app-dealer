0429789c032f541116f68b75e2755ec49c7b0681
Luke Wagner <LWagner@jaspergroup.us.com>
2026-06-15 11:14:37 -0400

    Rebalance resources screen into even 3-item categories

--- src/screens/resources/data.js
+++ src/screens/resources/data.js
@@ -3,30 +3,30 @@ export const RESOURCES_DATA = [
     {
         category: "Sales Tools",
         items: [
-            { label: "Sample Discounts", nav: "resources/sample-discounts" },
             { label: "Contracts", nav: "resources/contracts" },
+            { label: "Sample Discounts", nav: "resources/sample-discounts" },
+            { label: "Presentations", nav: "resources/presentations" },
         ].sort((a, b) => a.label.localeCompare(b.label))
     },
     {
         category: "Product & Finish Resources",
         items: [
-            { label: "Lead Times", nav: "resources/lead-times" },
-            { label: "Weight Ratings", nav: "resources/weight-ratings" },
             { label: "Search Fabrics", nav: "resources/search-fabrics" },
-            { label: "Request COM Yardage", nav: "resources/request-com-yardage" },
+            { label: "Weight Ratings", nav: "resources/weight-ratings" },
             { label: "Discontinued Finishes Database", nav: "resources/discontinued-finishes" },
         ].sort((a, b) => a.label.localeCompare(b.label))
     },
     {
         category: "Field Support",
         items: [
+            { label: "Lead Times", nav: "resources/lead-times" },
+            { label: "Request COM Yardage", nav: "resources/request-com-yardage" },
             { label: "Install Instructions", nav: "resources/install-instructions" },
         ].sort((a, b) => a.label.localeCompare(b.label))
     },
     {
         category: "Marketing & Communication",
         items: [
-            { label: "Presentations", nav: "resources/presentations" },
             { label: "Social Media", nav: "resources/social-media" },
             { label: "Tradeshows", nav: "resources/tradeshows" },
             { label: "LWYD Marketplace", nav: "marketplace", sublabel: "Rewards shop" },
